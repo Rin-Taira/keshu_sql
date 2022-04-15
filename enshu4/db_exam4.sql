eb92b3ada6999d1f0b95c575d6ed3c491aaec5c1
Rin-Taira <22taira_rin@axiz.co.jp>
2022-04-15 16:45:38 +0900

    完成

---
+++ enshu4/db_exam4.sql
@@ -0,0 +1,31 @@
+-- 1.インデックスの作成
+CREATE INDEX mn_index ON major(major_name);
+CREATE INDEX sn_index ON student(student_name);
+CREATE INDEX g_sn_index ON student(grade, student_name);
+
+-- 2.ビューの作成
+CREATE VIEW v_student AS
+SELECT student_id, student_name, hometown, major_name
+FROM student AS s
+JOIN major AS m
+ON s.major_id = m.major_id
+WHERE hometown <> '東京'
+ORDER BY major_name;
+
+-- 3.マテリアライズド・ビュー
+CREATE MATERIALIZED VIEW mv_student AS
+SELECT * FROM v_student;
+
+-- 4.ファンクション
+CREATE OR REPLACE FUNCTION ic_amount(amount integer) RETURNS integer
+LANGUAGE plpgsql
+AS $function$
+declare
+
+begin
+    return trunc(amount * 1.1, 0);
+end;
+$function$;
+
+SELECT *, ic_amount(amount)
+FROM sales AS s1;
